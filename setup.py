8a104ef8a56d861e4579d8d79fa355d98474970b
Hans Matos <hans.matos@pucp.edu.pe>
2020-10-26 14:08:14 -0500

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='text-complexity-analyzer-cm',
     author='Hans Matos Rios',
     author_email='hans.matos@pucp.edu.pe',
     version='0.1.0',
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -12,8 +13,6 @@ setup(
         'Pyphen',
         'scikit-learn'
     ],
-    packages=['model'],
-    package_dir={'model': 'text_complexity_analyzer_cm/model'},
     package_data={'model': ['*.pkl']}
 )
 

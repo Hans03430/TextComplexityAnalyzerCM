7334ba5621d679d22127756d502c55256347a8fb
Hans Matos <hans.matos@pucp.edu.pe>
2020-10-26 14:03:21 -0500

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     author='Hans Matos Rios',
     author_email='hans.matos@pucp.edu.pe',
     version='0.1.0',
-    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[

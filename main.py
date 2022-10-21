e014c7f210e003dbd8c7828e60e6e4681e116887
Hans Matos <hans.matos@pucp.edu.pe>
2022-10-20 20:32:42 -0500

    written documentation

---
+++ main.py
@@ -0,0 +1,24 @@
+from msilib.schema import Directory
+import os
+from text_complexity_analyzer_cm.constants import BASE_DIRECTORY
+ 
+def write_documentation(directory):
+    elements = os.listdir(directory)
+    # Iterate over the elements found
+    for element in elements:
+        module_name = '.'.join(directory.split('/')[7:])
+        print(module_name, Directory)
+        if os.path.isfile(f'{directory}/{element}'):
+            if element == '__init__.py': # Write documentation for module
+                os.system(f'python -m pydoc -w {module_name}')
+            else: # Write documentation for file
+                file_name_no_extension = element.replace('.py', '')
+                os.system(f'python -m pydoc -w {module_name}.{file_name_no_extension}')
+        elif os.path.isdir(f'{directory}/{element}') and element != '__pycache__':
+            write_documentation(f'{directory}/{element}')
+
+if __name__ == "__main__":
+    print(BASE_DIRECTORY)
+    os.chdir(f'{BASE_DIRECTORY}')
+    modules_path = f'{BASE_DIRECTORY}'
+    write_documentation(modules_path)
